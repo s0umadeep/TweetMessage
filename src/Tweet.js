a35b469b837cf40b9c5eb2d393a61bbf0b94a24e
Soumadeep <nicksouma@gmail.com>
2020-12-24 23:18:26 +0530

    Like button changed

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React,{useState}from "react";
 import "./App.css"
 
 function  Tweet(props) {
 
-    
+const [count,setcount]=useState(0);
+const increment =()=>{
+  setcount(count+1)
+}
     console.log("Tweet component has been hit ...")
     
     return(
@@ -11,16 +14,11 @@ function  Tweet(props) {
         <div className = "Tweet">
             <h3> {props.name}</h3>
             <p>  {props.message }</p>
-            <button onClick={counter}> Likes </button>
+            <button onClick={increment}> Likes </button>
+            <p>{count}</p>
             
         </div>
     
-    );
-}
-
-function counter(props){
-    return(
-        console.log(" Logged...")
     );
 }
 
